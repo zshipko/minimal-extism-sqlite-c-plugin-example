02d3a042a38632d7c9c31293b650d01b9b8152c0
zach <zach@dylib.so>
2023-04-17 13:31:14 -0700

    updates to get unixvfs working

--- unixvfs/main.c
+++ unixvfs/main.c
@@ -5,6 +5,7 @@
 #include "sqlite3ext.h"
 
 #include <errno.h>
+#include <sys/stat.h>
 
 // TODO: clean this up
 
@@ -12,7 +13,9 @@ static char *out_start;
 static char *out_head;
 
 static void errorLogCallback(void *pStatement, int iErrCode, const char *zMsg) {
-  out_head += snprintf(out_head, 1024-(out_head-out_start), "statement: %s\n(%d) %s\n",*(char**)pStatement, iErrCode, zMsg);
+  out_head += snprintf(out_head, 1024 - (out_head - out_start),
+                       "statement: %s\n(%d) %s\n", *(char **)pStatement,
+                       iErrCode, zMsg);
 }
 
 int lstat_ignore_enotcapable(const char *path, struct stat *buf) {
@@ -24,20 +27,15 @@ int lstat_ignore_enotcapable(const char *path, struct stat *buf) {
   return err;
 }
 
-int sqlite3_memvfs_init(
-  sqlite3 *db, 
-  char **pzErrMsg, 
-  const sqlite3_api_routines *pApi
-);
-int core_init(const char* dummy)
-{
-    int nErr = 0;
-    nErr += sqlite3_auto_extension((void *)sqlite3_memvfs_init);
-    return nErr ? SQLITE_ERROR : SQLITE_OK;
+int sqlite3_memvfs_init(sqlite3 *db, char **pzErrMsg,
+                        const sqlite3_api_routines *pApi);
+int core_init(const char *dummy) {
+  int nErr = 0;
+  nErr += sqlite3_auto_extension((void *)sqlite3_memvfs_init);
+  return nErr ? SQLITE_ERROR : SQLITE_OK;
 }
 
-int32_t count_vowels()
-{
+int32_t count_vowels() {
   uint64_t length = extism_input_length();
   uint8_t *input = malloc(length);
   extism_load_input(input, length);
@@ -65,10 +63,11 @@ int32_t count_vowels()
 
   sqlite3 *db;
 
-  rc = sqlite3_open("/path/to/your.db", &db);
+  rc = sqlite3_open("/path/to/test.db", &db);
   if (rc != SQLITE_OK) {
-    //int n = snprintf(out, 128, "open failed: %s (log size: %d)\n", sqlite3_errmsg(db), out_head-out_start);
-    int n = out_head-out_start;
+    // int n = snprintf(out, 128, "open failed: %s (log size: %d)\n",
+    // sqlite3_errmsg(db), out_head-out_start);
+    int n = out_head - out_start;
     uint64_t offs_ = extism_alloc(n);
     extism_store(offs_, (const uint8_t *)out, n);
     extism_output_set(offs_, n);
@@ -76,8 +75,8 @@ int32_t count_vowels()
   }
 
   sqlite3_stmt *stmt;
-  if (sqlite3_prepare_v2(db, "SELECT name FROM sqlite_schema", -1, &stmt, NULL) !=
-      SQLITE_OK) {
+  if (sqlite3_prepare_v2(db, "SELECT name FROM sqlite_schema", -1, &stmt,
+                         NULL) != SQLITE_OK) {
     int n = snprintf(out, 128, "prepare: %s\n", sqlite3_errmsg(db));
     uint64_t offs_ = extism_alloc(n);
     extism_store(offs_, (const uint8_t *)out, n);
